a4fd50336404e4d3395fbb6b00a6e2c50bbd0719
Max <vip.devyatilov@mailru>
2021-07-16 15:55:20 +0300

    Initial commit

---
+++ build/precache-manifest.f91e7bde38dd8d91830e62f9c08eab90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeaec88c4b7981bcf53a",
+    "url": "/static/css/main.92ff283f.chunk.css"
+  },
+  {
+    "revision": "aeaec88c4b7981bcf53a",
+    "url": "/static/js/main.aeaec88c.chunk.js"
+  },
+  {
+    "revision": "4fdc24d2c0d108b9833a",
+    "url": "/static/css/1.ac7adfcd.chunk.css"
+  },
+  {
+    "revision": "4fdc24d2c0d108b9833a",
+    "url": "/static/js/1.4fdc24d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "f3cfb6e030614f4ce0c8e6665dee4a66",
+    "url": "/index.html"
+  }
+];
